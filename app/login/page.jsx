27c399d8495dc9abd5460b02dc47a2b5723e71c1
şükrü <suko.crc06@gmail.com>
2026-08-02 04:51:05 +0300

    changed

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,7 +2,12 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabase";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export default function LoginPage() {
   const router = useRouter();
